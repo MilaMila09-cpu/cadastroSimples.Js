6c1c74e20f9e04b32b46705fa5febcbc4e4a5cbb
Milena Alana <milena.2021107887@gmail.com>
2025-08-27 20:19:15 -0300

    Add files via upload

---
+++ simples.js
@@ -0,0 +1,9 @@
+console.log("======== MENU ========");
+console.log("1 - Listar Clientes");
+console.log("2 - Cadastrar Cliente");
+console.log("3 - Editar Cliente");
+console.log("4 - Remover Cliente");
+console.log("0 - Sair do Sistema");
+console.log("======================");
+console.log("Opção: __");
+console.log("======================");
